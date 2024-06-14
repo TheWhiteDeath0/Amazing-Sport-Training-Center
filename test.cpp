53067498636c540a0cf00648d0f0f48ecdd31f8e
TheWhiteDeath0 <thantoeaung0977@gmail.com>
2024-06-15 02:04:03 +0630

    TheSum

--- test.cpp
+++ test.cpp
@@ -3,4 +3,11 @@ using namespace std;
 int  main(){
     //lol//
     cout <<"Hello World";
+    int h,z;
+    cout <<"ENter a number: ";
+    cin >>h>>z;
+    int x = h+z;
+    cout <<"The sum of the Numbers: "<<x;
+    
+    
 }
